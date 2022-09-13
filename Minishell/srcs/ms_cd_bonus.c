cb2f97a99fb87d8bd1f42dea18a372ca05bda923
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2022-09-13 20:43:01 +0800

    Small fix for cd

--- Minishell/srcs/ms_cd_bonus.c
+++ Minishell/srcs/ms_cd_bonus.c
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 21:04:30 by schuah            #+#    #+#             */
-/*   Updated: 2022/09/13 19:00:37 by schuah           ###   ########.fr       */
+/*   Updated: 2022/09/13 20:41:52 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 ** The directory is changed in the parent instead of in the child */
 int	check_cd_command(char *command, char *path)
 {
-	if (ft_strncmp(command, "cd", 8) != 0)
+	if (ft_strncmp(command, "cd", 3) != 0)
 		return (0);
 	if (path == NULL)
 	{
