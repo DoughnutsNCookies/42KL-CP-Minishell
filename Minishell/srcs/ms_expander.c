8fb7322a781cb4b771488f8dfc3e9828504e9d83
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2022-10-01 00:32:52 +0800

    Fix bracket

--- Minishell/srcs/ms_expander.c
+++ Minishell/srcs/ms_expander.c
@@ -108,7 +108,7 @@ t_list	*convert_quote(t_main *main, char *arg, t_list *current)
 				j = -1;
 				while (split[++j] != 0)
 				{
-					if (check_star(split[j])
+					if (check_star(split[j]))
 					{
 						files = get_files_from_dir(split[j]);
 						if (files == NULL)
