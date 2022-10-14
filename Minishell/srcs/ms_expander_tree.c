0d01a3ffef10387afa7d847423a1b5e21e80d491
Matthew <matthew.liew99@gmail.com>
2022-10-15 04:54:14 +0800

    expander fix

--- Minishell/srcs/ms_expander_tree.c
+++ Minishell/srcs/ms_expander_tree.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_expander_tree.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: maliew <maliew@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 16:19:28 by maliew            #+#    #+#             */
-/*   Updated: 2022/10/11 13:02:39 by schuah           ###   ########.fr       */
+/*   Updated: 2022/10/11 22:22:10 by maliew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,15 @@ void	ms_expander_delete_null(t_list **list)
 	if (*(char **)curr->content == NULL)
 	{
 		*list = curr->next;
+		free(curr->content);
+		free(curr);
+		curr = NULL;
+		ms_expander_delete_null(list);
+	}
+	else if (**(char **)curr->content == 0)
+	{
+		*list = curr->next;
+		free(*(char **)curr->content);
 		free(curr->content);
 		free(curr);
 		curr = NULL;
